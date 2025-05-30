e8f902cfb8a131b839f4b41de404e63b07e3cfa2
Ernício Jermias Chilundo <ernicio.j.chilundo@gmail.com>
2025-05-30 08:31:24 +0200

    criado a parte logica do projeto, ele leva apenas dois id e uma funcao que usa a biblioteca random para exibir frases motivacionais duma forma randomica

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+    const elementoQuote = document.getElementById('quote');
+    const button = document.getElementById('new-quote');
+
+    const quuotes = [
+        
+    ]
+})
