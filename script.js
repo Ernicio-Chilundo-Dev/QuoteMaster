6dff3a74067ec8bf9a4b08a0afb20f484e363c50
Ernício Jermias Chilundo <ernicio.j.chilundo@gmail.com>
2025-05-30 08:43:44 +0200

    array que contem as strings necessarias para dar um exemplo claro de como o  projeto ira funcionar pronto!

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ document.addEventListener('DOMContentLoaded', ()=>{
     const button = document.getElementById('new-quote');
 
     const quuotes = [
-        
+        "Acredite em você e tudo será possível.",
+        "Não espere por oportunidades, crie-as.",
+        "Grandes coisas nunca vêm da zona de conforto.",
+        "Você é mais forte do que imagina.",
+        "O sucesso é a soma de pequenos esforços diários.",
+        "Cada dia é uma nova chance de recomeçar."
     ]
 })
